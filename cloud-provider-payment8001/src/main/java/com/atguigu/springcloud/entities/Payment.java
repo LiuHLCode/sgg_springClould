c13439f60ab9040c43e28a35e71e79b8fe41a954
lhl <718560214@qq.com>
2023-01-05 10:03:03 +0800

    第十集 支付模块完成

---
+++ cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/entities/Payment.java
@@ -0,0 +1,14 @@
+package com.atguigu.springcloud.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+//添加有参构造和无参构造
+@AllArgsConstructor
+@NoArgsConstructor
+public class Payment {
+    private Long id;
+    private String serial;
+}
---
+++ cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/service/impl/PaymentServiceImpl.java
@@ -0,0 +1,31 @@
+package com.atguigu.springcloud.service.impl;
+
+import com.atguigu.springcloud.dao.PaymentDao;
+import com.atguigu.springcloud.entities.Payment;
+import com.atguigu.springcloud.service.PaymentService;
+import org.apache.ibatis.annotations.Param;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+
+/**
+ * @USER: Liuhl
+ * @DATE: 2022/12/30 15:55
+ */
+@Service
+public class PaymentServiceImpl implements PaymentService {
+    //@Resource=@AutoWired+@Qualifier
+    @Resource
+    private PaymentDao paymentDao;
+
+    @Override
+    public int create(Payment payment) {
+        return paymentDao.create(payment);
+    }
+
+    @Override
+    public Payment getPaymentById(Long id) {
+        return paymentDao.getPaymentById(id);
+    }
+}
